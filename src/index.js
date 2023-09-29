59c9ce9b976e889d7f934d9c2aaae701706d465a
Roald Christesen <roald@grain.one>
2023-09-29 19:15:57 +0200

    initial demonstration how to use @sndcds/mvc

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import App from './app.js'
+import AppModel from './appModel.js'
+import TestComponent from './testcomponent.js'
+import DistrictSelectComponent from './districtSelectComponent.js'
+import PopComponent from './popComponent.js'
+
+export {
+    App,
+    AppModel,
+    TestComponent,
+    DistrictSelectComponent,
+    PopComponent
+}
